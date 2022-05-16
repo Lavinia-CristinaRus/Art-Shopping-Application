9ee42d73905b49649f492742dbeb5016d91a2b2f
Lavinia-CristinaRus <lavinia.rus20@gmail.com>
2022-05-16 07:33:00 +0300

    ASA-49 create a GUI with a list view for the items

---
+++ src/main/java/ArtShoppingApplication/controllers/MyItemsController.java
@@ -0,0 +1,34 @@
+package ArtShoppingApplication.controllers;
+
+import ArtShoppingApplication.services.UserService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MyItemsController {
+    @FXML
+    private ListView items;
+    //@FXML
+    //private ListView description;
+    @FXML
+    private ListView price;
+
+    @FXML
+    public void openItemRegistrationForm(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(""));
+        Parent p = (Parent)fxmlLoader.load();
+        Scene scene0 = new Scene(p, 1000, 600);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setTitle("Add items page");
+        window.setScene(scene0);
+        window.show();
+    }
+
+}
