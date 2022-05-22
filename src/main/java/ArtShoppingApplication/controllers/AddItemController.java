dc117655259a1ed2058015f0f57a83d1c17c5655
Lavinia-CristinaRus <lavinia.rus20@gmail.com>
2022-05-22 06:50:20 +0300

    ASA-13 Select picture method

--- src/main/java/ArtShoppingApplication/controllers/AddItemController.java
+++ src/main/java/ArtShoppingApplication/controllers/AddItemController.java
@@ -1,4 +1,66 @@
 package ArtShoppingApplication.controllers;
 
+import ArtShoppingApplication.exceptions.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import ArtShoppingApplication.services.UserService;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+
 public class AddItemController {
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField description;
+    @FXML
+    private TextField price;
+    @FXML
+    private TextField category;
+    @FXML
+    private TextField dimensions;
+    @FXML
+    private TextField materials;
+    @FXML
+    private TextField colors;
+    @FXML
+    private TextField weight;
+    @FXML
+    private ImageView picture;
+    Image fxImage;
+    @FXML
+    public void handleSelectPicture() {
+
+        Stage stage = (Stage) picture.getScene().getWindow();
+
+        FileChooser file = new FileChooser();
+        file.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+        File selected = file.showOpenDialog(stage);
+
+
+        fxImage = new Image(new File(selected.getAbsolutePath()).toURI().toString());
+        picture.setImage(fxImage);
+    }
+
+    public void handleAddItem() {
+    }
+
+    public void handleCancel() {
+    }
+
 }
